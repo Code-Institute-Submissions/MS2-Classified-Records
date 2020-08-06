6e01bdf66861606e83d0ed663b5052abf817ce42
Dermot-K <dermokeelan@gmail.com>
2020-08-06 16:32:59 +0000

    added google maps script and initMap function

---
+++ assets/js/map.js
@@ -0,0 +1,9 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 12,
+        center: {
+            lat: 54.003053,
+            lng: 6.403441
+        }
+    });
+}

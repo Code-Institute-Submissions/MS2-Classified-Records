afa3e78deb9652a78b95ea8da0db0b44b1a75337
Dermot-K <dermokeelan@gmail.com>
2020-08-11 17:38:07 +0000

    add function to toggle album review on album cover mouseenter/out

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,3 +26,27 @@ document.getElementById("contact-form").addEventListener("submit", resetForm);
 function resetForm() {
     document.getElementById("contact-form").reset();
 }
+
+//Fade in/out album reviews on mouseover/out
+const toggleAlbumReview = () => {
+    const albumCover = document.querySelector(".album-cover");
+    const albumReview = document.querySelector(".album-review");
+    
+    albumCover.addEventListener("mouseenter", function(){
+        albumReview.classList.remove("fade-out");
+        albumReview.classList.add("fade-in");
+    });
+
+    albumCover.addEventListener("mouseout", function(){
+        albumReview.classList.remove("fade-in");
+        albumReview.classList.add("fade-out");
+    });
+
+    albumReview.addEventListener("mouseenter", function(){
+        albumReview.classList.remove("fade-out");
+        albumReview.classList.add("fade-in");
+    });
+
+}
+toggleAlbumReview();
+

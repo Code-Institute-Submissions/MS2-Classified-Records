04545e0a157672c08f6b0a7867ff3a1fad37ccdb
Dermot-K <dermokeelan@gmail.com>
2020-08-13 16:55:50 +0000

    add comments to file

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,13 +1,16 @@
 function initMap() {
+    //set initial location to store location
     var storeLocation = {
             lat: 54.003053,
             lng: -6.403441
         };
-
+    
+    //set zoom level on map
     var map = new google.maps.Map(document.getElementById("map"), {
         zoom: 16,
         center: storeLocation
         });
     
+    //add marker to map at store location
     var marker = new google.maps.Marker({position: storeLocation, map: map});
 }

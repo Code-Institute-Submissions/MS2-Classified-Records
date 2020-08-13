ad0b75211892d26ff43002808479fb8b1572988b
Dermot-K <dermokeelan@gmail.com>
2020-08-13 16:50:44 +0000

    add comments

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,4 +1,6 @@
 function sendMail(contactForm){
+    //Email JS template details
+    //takes values from form inputs and sends queries to test email address 
     emailjs.send("keelan.dev.test@gmail.com", "classified_records", {
         "from_name": contactForm.name1.value,
         "from_email":contactForm.email1.value,
